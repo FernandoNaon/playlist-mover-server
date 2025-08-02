41c586138860e81188456a690556fde4dbe587cd
Fernando Naon <fernandonaon@gmail.com>
2025-08-02 14:50:24 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask, request, redirect, session, url_for, jsonify
+from flask_cors import CORS
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+app = Flask(__name__)
+CORS(app, supports_credentials=True)
+app.secret_key = os.environ.get("FLASK_SECRET_KEY", "supersecretkey")
+
+SPOTIPY_CLIENT_ID = os.environ.get("SPOTIPY_CLIENT_ID")
+SPOTIPY_CLIENT_SECRET = os.environ.get("SPOTIPY_CLIENT_SECRET")
+SPOTIPY_REDIRECT_URI = os.environ.get("SPOTIPY_REDIRECT_URI", "http://127.0.0.1:5000/callback")
+
+FRONTEND_REDIRECT = "http://localhost:5173/callback"  
+
+SCOPE = "playlist-read-private playlist-read-collaborative"
+
+def get_spotify_oauth():
+    return SpotifyOAuth(
+        client_id=SPOTIPY_CLIENT_ID,
+        client_secret=SPOTIPY_CLIENT_SECRET,
+        redirect_uri=SPOTIPY_REDIRECT_URI,
+        scope=SCOPE
+    )
+
+@app.route("/login")
+def login():
+    auth_url = get_spotify_oauth().get_authorize_url()
+    return jsonify({"auth_url": auth_url})
+
+@app.route("/callback")
+def spotify_callback_redirect():
+    code = request.args.get("code")
+    return redirect(f"{FRONTEND_REDIRECT}?code={code}")
+
+@app.route("/callback", methods=["POST"])
+def spotify_callback_exchange():
+    data = request.get_json()
+    code = data.get("code")
+
+    sp_oauth = get_spotify_oauth()
+    token_info = sp_oauth.get_access_token(code)
+
+    sp = spotipy.Spotify(auth=token_info['access_token'])
+    playlists_data = sp.current_user_playlists(limit=50)
+
+    playlists = [{
+        "id": p["id"],
+        "name": p["name"],
+        "tracks_total": p["tracks"]["total"]
+    } for p in playlists_data["items"]]
+
+    return jsonify(playlists)
+
+if __name__ == "__main__":
+    app.run(debug=True)
